352e9e1102103b958b24dbfe3bc44f4a4aee184a
tanzim <tanzim3421@gmail.com>
2020-12-04 00:13:52 +0600

    Fixed Crashing

--- app/src/main/java/com/example/back4apptester/MainActivity.java
+++ app/src/main/java/com/example/back4apptester/MainActivity.java
@@ -30,6 +30,7 @@ import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,10 +45,10 @@ public class MainActivity extends AppCompatActivity {
         try {
             parseLiveQueryClient = ParseLiveQueryClient.Factory.getClient(new URI("wss://livequery.b4a.io/"));
         } catch (URISyntaxException e) {
-            Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
         }
 
-        if(parseLiveQueryClient!=null){
+        if (parseLiveQueryClient != null) {
             Toast.makeText(this, "Client established", Toast.LENGTH_SHORT).show();
             //Subscribe
             ParseQuery<ParseObject> parseQuery = new ParseQuery("Dogs");
@@ -58,9 +59,9 @@ public class MainActivity extends AppCompatActivity {
 
                 Handler handler = new Handler(Looper.getMainLooper());
                 handler.post(() -> {
-                    textView.setText("Created "+object.getObjectId());
+                    textView.setText("Created " + object.getObjectId());
                     Toast.makeText(MainActivity.this, "Created", Toast.LENGTH_SHORT).show();
-                    Log.d("custom","created");
+                    Log.d("custom", "created");
                 });
 
             });
@@ -68,62 +69,13 @@ public class MainActivity extends AppCompatActivity {
             subscriptionHandling.handleEvent(SubscriptionHandling.Event.UPDATE, (query, object) -> {
                 Handler handler = new Handler(Looper.getMainLooper());
                 handler.post(() -> {
-                textView.setText("Updated "+object.getObjectId());
-                Toast.makeText(MainActivity.this, "Updated", Toast.LENGTH_SHORT).show();
-                Log.d("custom","updated");
+                    textView.setText("Updated " + object.getObjectId());
+                    Toast.makeText(MainActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+                    Log.d("custom", "updated");
                 });
 
             });
         }
 
     }
-
-//    public void signUp(View view) {
-//        ParseUser user = new ParseUser();
-//        user.setUsername("01714251278");
-//        user.setPassword("6693");
-//        user.signUpInBackground(e -> {
-//            if (e==null){
-//                Toast.makeText(MainActivity.this, "Compete", Toast.LENGTH_SHORT).show();
-//            }else{
-//                Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
-//
-//    public void logOut(View view) {
-//        ParseUser.logOutInBackground(e -> {
-//            if (e==null){
-//                Toast.makeText(MainActivity.this, "Logged out", Toast.LENGTH_SHORT).show();
-//            }else{
-//                Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
-//
-//    public void login(View view) {
-//        ParseUser.logInInBackground("01714251278", "8940", (user, e) -> {
-//            if (e==null){
-//                Toast.makeText(MainActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
-//            }else{
-//                Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
-//
-//    public void fetch(View view) {
-//        ParseQuery<ParseObject> query = ParseQuery.getQuery("Dogs");
-//
-//        // The query will search for a ParseObject, given its objectId.
-//        // When the query finishes running, it will invoke the GetCallback
-//        // with either the object, or the exception thrown
-//        query.getInBackground("TJAXLGFZSp", (result, e) -> {
-//            if (e == null) {
-//                Toast.makeText(this, "Dog "+result.getString("name"), Toast.LENGTH_SHORT).show();
-//            } else {
-//                // something went wrong
-//                Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
 }
